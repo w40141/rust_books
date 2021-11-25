1ed71cde26d5acdd9499527a7803771982f78e95
w40141 <w.40141@gmail.com>
2021-11-25 09:54:51 +0900

    finish sec. 2-5

--- sec2/src/main.rs
+++ sec2/src/main.rs
@@ -1,3 +1,14 @@
+use std::convert::TryInto;
+
 fn main() {
-    println!("Hello, world!");
+    let a: i32 = 10;
+    let b: u16 = 100;
+
+    let b_ = b.try_into().unwrap();
+
+    if a > b_ {
+        println!("{}", 1);
+    } else {
+        println!("{}", 2);
+    }
 }
