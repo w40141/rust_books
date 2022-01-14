86f654c2621fed49e47b895ca6ab5c64860d0597
w40141 <w.40141@gmail.com>
2022-01-14 13:53:01 +0900

    finish sec_06

--- sec_06/src/pointer_test.rs
+++ sec_06/src/pointer_test.rs
@@ -1,5 +1,8 @@
+use std::mem;
+
 pub fn main() {
     let a: i64 = 42;
     let a_ptr = &a as *const i64;
-    println!("a: {} ({:p})", a, a_ptr);
+    let a_addr: usize = unsafe { mem::transmute(a_ptr) };
+    println!("a: {} ({:p}...0x{:x})", a, a_ptr, a_addr + 7);
 }
