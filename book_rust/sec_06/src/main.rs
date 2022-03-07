8a25f4b89b1d5b7d08a0218ce35bada5d9a54f0a
w40141 <w.40141@gmail.com>
2022-03-07 10:18:11 +0900

    finish sec_06.4.1

--- book_rust/sec_06/src/main.rs
+++ book_rust/sec_06/src/main.rs
@@ -6,9 +6,11 @@ mod r#macro;
 pub mod sec_1;
 pub mod sec_2;
 pub mod sec_3;
+pub mod sec_4;
 
 fn main() {
     // sec_1::sec_01();
     // sec_2::sec_02();
-    sec_03()
+    // sec_03()
+    // println!("{}", calc::eval("2+5").unwrap());
 }
