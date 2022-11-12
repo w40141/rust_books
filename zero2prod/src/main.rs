e696e08649a3bccdb47f67e1039841393f7d216c
w40141 <w.40141@gmail.com>
2022-11-13 01:19:36 +0900

    finish sec 3.8.2.5

--- zero2prod/src/main.rs
+++ zero2prod/src/main.rs
@@ -1,31 +1,9 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse};
+use std::net::TcpListener;
 
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
-}
-
-async fn health_check() -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
+use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-        .route("/health_check", web::get().to(health_check))
-    })
-    .bind("127.0.0.1:8000")?
-    .run().await
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[tokio::test]
-    async fn health_check_test() {
-        let resp = health_check().await;
-        assert!(resp.status().is_success())
-    }
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind random port");
+    run(listener)?.await
 }
